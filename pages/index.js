2c72d6399243ca7b25f51eddc71ecbdb09bbadf8
AJ May <aj7may@gmail.com>
2022-02-22 19:07:25 -0500

    fix title and change copy

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,9 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>AJ Coin</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main className={styles.main}>
         <div class="nes-container with-title is-centered">
-          <p class="title">Container.is-centered</p>
+          <p class="title">AJ Coin</p>
           <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
         </div>
       </main>
