151561cef60042d36448e92555e6620db848476b
AJ May <aj7may@gmail.com>
2022-02-28 22:07:05 -0500

    feat: add sushi link

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
           <a href="https://github.com/aj-may/ajcoin" target="_blank" style={{ color: 'gray', margin: '1rem' }}>github</a>
           <a href="/whitepaper.pdf" target="_blank" style={{ color: 'gray', margin: '1rem' }}>whitepaper</a>
           <a href="https://polygonscan.com/address/0x0DcBfa223d69edadB70c90F12B014fcFF8D281b2" target="_blank" style={{ color: 'gray', margin: '1rem' }}>polygonscan</a>
+          <a href="https://polygon.sushi.com/en/swap?inputCurrency=0x0DcBfa223d69edadB70c90F12B014fcFF8D281b2" target="_blank" style={{ color: 'gray', margin: '1rem' }}>sushi</a>
         </p>
       </main>
     </div>
