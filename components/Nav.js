cd095648f34f39cc847e51ae0dce28989d2c6bcc
AJ May <aj7may@gmail.com>
2022-03-14 17:03:10 -0400

    update nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,6 +1,11 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function Nav() {
+  const { pathname } = useRouter();
+  const isHome = pathname === '/';
+  const isApp = pathname.startsWith('/app');
+
   return <div className="flex justify-between items-center px-4 py-6 sm:px-6 md:justify-start md:space-x-10">
     <div className="flex justify-start lg:w-0 lg:flex-1">
       <Link href="/">
@@ -13,7 +18,8 @@ export default function Nav() {
         </a>
       </Link>
     </div>
-    <nav className="hidden md:flex space-x-10">
+
+    {isHome && <nav className="hidden md:flex space-x-10">
       <a href="#about" className="text-base font-medium text-gray-500 hover:text-gray-900">
         About
       </a>
@@ -23,13 +29,16 @@ export default function Nav() {
       <a href="#team" className="text-base font-medium text-gray-500 hover:text-gray-900">
         Team
       </a>
-    </nav>
+    </nav>}
+
+    {isApp && <nav className="hidden md:flex space-x-10"></nav>}
+
     <div className="md:flex items-center justify-end md:flex-1 lg:w-0">
-      <Link href="/app">
+      {isHome && <Link href="/app">
         <a className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
           Launch App
         </a>
-      </Link>
+      </Link>}
     </div>
   </div>;
 }
